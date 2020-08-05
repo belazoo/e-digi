4db98a6ec507abaf32a199d26c53e33d383c418e
Isabella Leal <isabellaleal016@gmail.com>
2020-08-05 15:47:26 -0300

    alterando dados do console

--- Livraria/src/Main/Main.java
+++ Livraria/src/Main/Main.java
@@ -7,18 +7,8 @@ import control.Controller;
 public class Main {
 	public static void main(String[] args) {
 		Controller c = new Controller();
-		Scanner sc1 = new Scanner(System.in);
-		Scanner sc2 = new Scanner(System.in);
-		
-		String nome;
-		String email;
-		
-		System.out.println("Insira o nome do autor: ");
-		nome = sc1.next();		
-		
-		System.out.println("Insira o email do autor: ");
-		email = sc2.next();
-		
+		String nome = "fulano";
+		String email = "fulano@gmail.com";
 		try {
 			c.cadastro(nome, email);
 		} catch (Exception e) {
